10b4546dd9a9039798ee1905f60feefdc974f2dd
devinw5500 <devinw5500@gmail.com>
2020-07-22 14:01:36 -0400

    Create Records works

--- NASS_Redo_Site/Controllers/HomeController.cs
+++ NASS_Redo_Site/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using NASS_Redo_Site.Models;
 
@@ -31,6 +32,7 @@ namespace NASS_Redo_Site.Controllers
         {
             return View();
         }
+        //Posting method for create person, takes all variables for basic record creating
         [HttpPost]
         public IActionResult CreatePerson(string FNameText, string MIText, string LNameText, string DOBText, 
             string ContactTypeSelect, bool PrimaryCheck, string ContactText,
@@ -38,6 +40,7 @@ namespace NASS_Redo_Site.Controllers
             string ZipSelect, string CitySelect, string CountySelect,
             string ParcelText)
         {
+            var ctx = new NASS_RedoContext();
             ViewData["FNameText"] = FNameText;
             ViewData["MIText"] = MIText;
             ViewData["LNameText"] = LNameText;
@@ -66,11 +69,181 @@ namespace NASS_Redo_Site.Controllers
                 "\nParcel: " + ParcelText);
 
             string newperson;
+
+            newperson = "\nDECLARE @RC int" +
+                        "\nDECLARE @FName varchar(50)" +
+                        "\nDECLARE @MInitial varchar(1)" +
+                        "\nDECLARE @LName varchar(50)" +
+                        "\nDECLARE @dob date" +
+                        "\nDECLARE @CityName varchar(50)" +
+                        "\nDECLARE @CountyName varchar(50)" +
+                        "\nDECLARE @ZipNum varchar(10)" +
+                        "\nSET @FName = \'" + FNameText + "\'" +
+                        "\nSET @MInitial = \'" + MIText + "\'" +
+                        "\nSET @LName = \'" + LNameText + "\'" +
+                        "\nSET @dob = \'" + DOBText + "\'" +
+                        "\nSET @CityName = \'" + CitySelect + "\'" +
+                        "\nSET @CountyName = \'" + CountySelect + "\'" +
+                        "\nSET @ZipNum = \'" + ZipSelect + "\'" +
+                        "\nEXECUTE @RC = [dbo].[CreateNewPersonBasics]" +
+                        "\n@FName" +
+                        "\n,@MInitial" +
+                        "\n,@LName" +
+                        "\n,@dob" +
+                        "\n,@CityName" +
+                        "\n,@CountyName" +
+                        "\n,@ZipNum";
+
             string newpersoncontact;
+
+            switch (ContactTypeSelect)
+            {
+                case "Email":
+                    newpersoncontact = "\nDECLARE @RC int" +
+                        "\nDECLARE @FName varchar(50)" +
+                        "\nDECLARE @MInitial varchar(1)" +
+                        "\nDECLARE @LName varchar(50)" +
+                        "\nDECLARE @dob date" +
+                        "\nDECLARE @Email varchar(100)" +
+                        "\nDECLARE @Prim bit" +
+                        "\nSET @FName = \'" + FNameText + "\'" +
+                        "\nSET @MInitial = \'" + MIText + "\'" +
+                        "\nSET @LName = \'" + LNameText + "\'" +
+                        "\nSET @dob = \'" + DOBText + "\'" +
+                        "\nSET @Email = \'" + ContactText + "\'" +
+                        "\nSET @Prim = \'" + PrimaryCheck + "\'" +
+                        "\nEXECUTE @RC = [dbo].[CreateNewPersonEmail]" +
+                        "\n@FName" +
+                        "\n,@MInitial" +
+                        "\n,@LName" +
+                        "\n,@dob" +
+                        "\n,@Email" +
+                        "\n,@Prim";
+                    break;
+
+                case "Phone":
+                    newpersoncontact = "\nDECLARE @RC int" +
+                        "\nDECLARE @FName varchar(50)" +
+                        "\nDECLARE @MInitial varchar(1)" +
+                        "\nDECLARE @LName varchar(50)" +
+                        "\nDECLARE @dob date" +
+                        "\nDECLARE @Email varchar(100)" +
+                        "\nDECLARE @Type varchar(20)" +
+                        "\nDECLARE @Prim bit" +
+                        "\nSET @FName = \'" + FNameText + "\'" +
+                        "\nSET @MInitial = \'" + MIText + "\'" +
+                        "\nSET @LName = \'" + LNameText + "\'" +
+                        "\nSET @dob = \'" + DOBText + "\'" +
+                        "\nSET @Email = \'" + ContactText + "\'" +
+                        "\nSET @Type = \'" + ContactTypeSelect + "\'" +
+                        "\nSET @Prim = \'" + PrimaryCheck + "\'" +
+                        "\nEXECUTE @RC = [dbo].[CreateNewPersonPhone]" +
+                        "\n@FName" +
+                        "\n,@MInitial" +
+                        "\n,@LName" +
+                        "\n,@dob" +
+                        "\n,@Email" +
+                        "\n,@Type" +
+                        "\n,@Prim";
+                    break;
+
+                case "Fax":
+                    newpersoncontact = "\nDECLARE @RC int" +
+                        "\nDECLARE @FName varchar(50)" +
+                        "\nDECLARE @MInitial varchar(1)" +
+                        "\nDECLARE @LName varchar(50)" +
+                        "\nDECLARE @dob date" +
+                        "\nDECLARE @Email varchar(100)" +
+                        "\nDECLARE @Type varchar(20)" +
+                        "\nDECLARE @Prim bit" +
+                        "\nSET @FName = \'" + FNameText + "\'" +
+                        "\nSET @MInitial = \'" + MIText + "\'" +
+                        "\nSET @LName = \'" + LNameText + "\'" +
+                        "\nSET @dob = \'" + DOBText + "\'" +
+                        "\nSET @Email = \'" + ContactText + "\'" +
+                        "\nSET @Type = \'" + ContactTypeSelect + "\'" +
+                        "\nSET @Prim = \'" + PrimaryCheck + "\'" +
+                        "\nEXECUTE @RC = [dbo].[CreateNewPersonPhone]" +
+                        "\n@FName" +
+                        "\n,@MInitial" +
+                        "\n,@LName" +
+                        "\n,@dob" +
+                        "\n,@Email" +
+                        "\n,@Type" +
+                        "\n,@Prim";
+                    break;
+
+                default:
+                    Debug.WriteLine("SWITCH AT DEFAULT");
+                    newpersoncontact = "oops";
+                    break;
+            }
+
             string newaddress;
+
+            newaddress = "\nDECLARE @RC int" +
+                        "\nDECLARE @CityName varchar(50)" +
+                        "\nDECLARE @CountyName varchar(50)" +
+                        "\nDECLARE @ZipNum varchar(10)" +
+                        "\nSET @CityName = \'" + CitySelect + "\'" +
+                        "\nSET @CountyName = \'" + CountySelect + "\'" +
+                        "\nSET @ZipNum = \'" + ZipSelect + "\'" +
+                        "\nEXECUTE @RC = [dbo].[CreateNewAddress]" +
+                        "\n@CityName" +
+                        "\n,@CountyName" +
+                        "\n,@ZipNum";
+
             string newaddressline;
 
-            return RedirectToAction("Index", "People");
+            newaddressline = "\nDECLARE @RC int" +
+                        "\nDECLARE @AddrLine varchar(50)" +
+                        "\nDECLARE @CityName varchar(50)" +
+                        "\nDECLARE @CountyName varchar(50)" +
+                        "\nDECLARE @ZipNum varchar(10)" +
+                        "\nSET @AddrLine = \'" + AddrLine1Text + "\'" +
+                        "\nSET @CityName = \'" + CitySelect + "\'" +
+                        "\nSET @CountyName = \'" + CountySelect + "\'" +
+                        "\nSET @ZipNum = \'" + ZipSelect + "\'" +
+                        "\nEXECUTE @RC = [dbo].[CreateNewAddressLine]" +
+                        "\n@Addrline" +
+                        "\n,@CityName" +
+                        "\n,@CountyName" +
+                        "\n,@ZipNum";
+
+            Debug.WriteLine("\nNewPerson: " + newperson + "\n");
+            Debug.WriteLine("\nNewPersonContact: " + newpersoncontact + "\n");
+            Debug.WriteLine("\nNewAddress: " + newaddress + "\n");
+            Debug.WriteLine("\nNewAddressLine: " + newaddressline + "\n");
+
+            ctx.Database.ExecuteSqlRaw(newaddress);
+            ctx.Database.ExecuteSqlRaw(newaddressline);
+            ctx.Database.ExecuteSqlRaw(newperson);
+            ctx.Database.ExecuteSqlRaw(newpersoncontact);
+
+            if (!AddrLine2Text.Equals(null))
+            {
+                string newaddressline2;
+
+                newaddressline2 = "\nDECLARE @RC int" +
+                            "\nDECLARE @AddrLine varchar(50)" +
+                            "\nDECLARE @CityName varchar(50)" +
+                            "\nDECLARE @CountyName varchar(50)" +
+                            "\nDECLARE @ZipNum varchar(10)" +
+                            "\nSET @AddrLine = \'" + AddrLine2Text + "\'" +
+                            "\nSET @CityName = \'" + CitySelect + "\'" +
+                            "\nSET @CountyName = \'" + CountySelect + "\'" +
+                            "\nSET @ZipNum = \'" + ZipSelect + "\'" +
+                            "\nEXECUTE @RC = [dbo].[CreateNewAddressLine]" +
+                            "\n@Addrline" +
+                            "\n,@CityName" +
+                            "\n,@CountyName" +
+                            "\n,@ZipNum";
+                Debug.WriteLine("\nNewAddressLine2: " + newaddressline2 + "\n");
+                ctx.Database.ExecuteSqlRaw(newaddressline2);
+            }
+
+
+            return RedirectToAction("Index", "PersonInfoes");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ NASS_Redo_Site/Controllers/PersonInfoesController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NASS_Redo_Site.Models;
+
+namespace NASS_Redo_Site.Controllers
+{
+    public class PersonInfoesController : Controller
+    {
+        private readonly NASS_RedoContext _context;
+
+        public PersonInfoesController(NASS_RedoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: PersonInfoes
+        public async Task<IActionResult> Index()
+        {
+            var nASS_RedoContext = _context.PersonInfo.Include(p => p.Addr).Include(p => p.Person);
+            return View(await nASS_RedoContext.ToListAsync());
+        }
+
+        // GET: PersonInfoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var personInfo = await _context.PersonInfo
+                .Include(p => p.Addr)
+                .Include(p => p.Person)
+                .FirstOrDefaultAsync(m => m.PersonInfoId == id);
+            if (personInfo == null)
+            {
+                return NotFound();
+            }
+
+            return View(personInfo);
+        }
+
+        // GET: PersonInfoes/Create
+        public IActionResult Create()
+        {
+            ViewData["AddrId"] = new SelectList(_context.Address, "AddressId", "AddressId");
+            ViewData["PersonId"] = new SelectList(_context.Person, "PersonId", "PersonId");
+            return View();
+        }
+
+        // POST: PersonInfoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PersonInfoId,PersonId,AddrId,FirstName,MiddleInitial,LastName,Dob")] PersonInfo personInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(personInfo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AddrId"] = new SelectList(_context.Address, "AddressId", "AddressId", personInfo.AddrId);
+            ViewData["PersonId"] = new SelectList(_context.Person, "PersonId", "PersonId", personInfo.PersonId);
+            return View(personInfo);
+        }
+
+        // GET: PersonInfoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var personInfo = await _context.PersonInfo.FindAsync(id);
+            if (personInfo == null)
+            {
+                return NotFound();
+            }
+            ViewData["AddrId"] = new SelectList(_context.Address, "AddressId", "AddressId", personInfo.AddrId);
+            ViewData["PersonId"] = new SelectList(_context.Person, "PersonId", "PersonId", personInfo.PersonId);
+            return View(personInfo);
+        }
+
+        // POST: PersonInfoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PersonInfoId,PersonId,AddrId,FirstName,MiddleInitial,LastName,Dob")] PersonInfo personInfo)
+        {
+            if (id != personInfo.PersonInfoId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(personInfo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PersonInfoExists(personInfo.PersonInfoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AddrId"] = new SelectList(_context.Address, "AddressId", "AddressId", personInfo.AddrId);
+            ViewData["PersonId"] = new SelectList(_context.Person, "PersonId", "PersonId", personInfo.PersonId);
+            return View(personInfo);
+        }
+
+        // GET: PersonInfoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var personInfo = await _context.PersonInfo
+                .Include(p => p.Addr)
+                .Include(p => p.Person)
+                .FirstOrDefaultAsync(m => m.PersonInfoId == id);
+            if (personInfo == null)
+            {
+                return NotFound();
+            }
+
+            return View(personInfo);
+        }
+
+        // POST: PersonInfoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var personInfo = await _context.PersonInfo.FindAsync(id);
+            _context.PersonInfo.Remove(personInfo);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PersonInfoExists(int id)
+        {
+            return _context.PersonInfo.Any(e => e.PersonInfoId == id);
+        }
+    }
+}
